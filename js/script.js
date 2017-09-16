87a47a32eb4e14687c0939e5a4a65b2fe9933629
nabilkhalil <duomaster01@gmail.com>
2017-09-15 22:21:11 -0700

    asdsad

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var myApp = angular.module('myApp',['ui.router']);
+//Website Routes
 myApp.config(function($stateProvider,$urlRouterProvider) {
     $urlRouterProvider.otherwise('/login');
  	$stateProvider.state('login', {
@@ -18,22 +19,39 @@ myApp.config(function($stateProvider,$urlRouterProvider) {
 
 });
 
+//Logged In Cookie
+document.loggedIn;
+
+//Login Controller
 myApp.controller('login', ['$scope','$http','$state','$location','$timeout',function($scope,$http,$state,$location,$timeout){
 
 	var usernamePass;
 	var passwordPass;
 	var logInButton = document.getElementById('logIn');
 	
+	//Gets password crendentials
 	$http.get('/credentials').then(function(data){
 			usernamePass = data.data.username;
 			passwordPass = data.data.password;
 	})
 	
+	//Checks is user has logged in or not
+	document.loggedIn = 'false';
+	if(document.loggedIn === 'false'){
+		$state.go('login');
+		console.log("your aren't logged in");
+	}else{
+		console.log("you're logged in")
+	}
+
+	console.log(document.loggedIn);
+
 
 	logInButton.onclick = function($location){
 		var usernameInput = document.getElementById('username').value;
 		var passwordInput = document.getElementById('password').value;
 		if(usernameInput == usernamePass & passwordInput == passwordPass){
+			document.loggedIn = 'true';
 			$state.go('trafficHistory');
 
 		}else{
@@ -45,11 +63,27 @@ myApp.controller('login', ['$scope','$http','$state','$location','$timeout',func
 
 }]);
 
+//Traffic History Controller
 myApp.controller('trafficHistory', ['$scope','$location','$http','$rootScope','$state', function($scope,$location,$http,$rootScope,$state){
 	$http.get('/user').then(function(data) {
 		$scope.users = data.data;
-		console.log(data.data)
 	})
+
+	var logOutButton = document.getElementById('logOutButton');
+	
+	logOutButton.onclick = function(){
+		document.loggedIn = 'false';
+	}
+
+	console.log(document.loggedIn);
+
+	if(document.loggedIn === 'false'){
+		$state.go('login');
+		console.log("your aren't logged in");
+	}else{
+		document.loggedIn = 'true';
+		console.log("you're logged in")
+	}
 
 }]);
 
